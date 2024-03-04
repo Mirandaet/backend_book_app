bcf2a2a4fc78128f30f590a84368c62d90d8eff2
Mirandaet <miranda.thulstrup@gmail.com>
2024-03-04 11:21:17 +0100

    started db models

--- app/database/models.py
+++ app/database/models.py
@@ -6,15 +6,46 @@ class base(DeclarativeBase):
     id:Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     
 class User(base):
+    __tablename__= "users"
     email: Mapped[str]
     password: Mapped[str]
     user_name: Mapped[str]
     book_goal: Mapped[int]
 
-class Books(base):
+class Category(base):
+    __tablename__= "categories"
+    name: Mapped[str]
+    color_code: Mapped[str]
+
+class Book(base):
+    __tablename__= "books"
     title: Mapped[str]
     page_count: Mapped[int]
+    is_ebook: Mapped[bool]
+    main_category: [int]
 
-class Types(base):
-    type_name: Mapped[str]
+    #relationships
+    
+
+class SubCategory:
+    __tablename__= "sub_categories"
+    category_id: Mapped[int]
+    book_id: Mapped[int]
+
+class BooksShelf:
+    __tablename__= "book_shelves"
+    user_id: Mapped[int]
+    book_id: Mapped[int]
+    pages_read: Mapped[int]
+    is_read: Mapped[bool]
+
+class Achievement:
+    __tablename__= "achievements"
+    name: Mapped[str]
+
+class CompletedAchievement:
+    __tablename__= "completed_achievements"
+    achievements_id: Mapped[int]
+    user_id: Mapped[int]
+
 
