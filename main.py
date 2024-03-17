a9454467eb264beb066015c78083fcd3cbc57393
Giada Unger <giada.unger02@gmail.com>
2024-03-17 12:26:08 +0100

    Corrected a typo in the add user endpoint

--- main.py
+++ main.py
@@ -144,11 +144,11 @@ def list_read_books(
 @app.post("/user", status_code=201)
 def add_user(user: PasswordSchema, db: Session = Depends(get_db)) -> PasswordSchema:
     hashed_password = get_password_hash(user.password)
-    new_user = User(email=user.email, user_name=user.user_name, password=hashed_password, book_goal=user.book_goal)
+    new_user = User(user_name=user.user_name, email=user.email, book_goal=user.book_goal, password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return PasswordSchema(username=new_user.user_name, email=new_user.email, book_goal=new_user.book_goal, password=hashed_password)
+    return PasswordSchema(user_name=new_user.user_name, email=new_user.email, book_goal=new_user.book_goal, password=hashed_password)
 
 
 @app.get("/categories", status_code=200)
